de2aca37d001e10a1eb6659404f9b8195001cd7b
Alex <alex.auternaud@inria.fr>
2020-02-20 17:11:10 +0100

    The number of Ros Gazebo Parall Sim can be chosen

--- launch/gazeboSim0.sh
+++ launch/gazeboSim0.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
-export ROS_MASTER_URI=http://localhost:11350
-export GAZEBO_MASTER_URI=http://localhost:11340
-roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world #&
-# roslaunch turtlebot_teleop keyboard_teleop.launch
+ros_localhost=11350
+gazebo_localhost=11450
+# export ROS_MASTER_URI=http://localhost:11350
+# export GAZEBO_MASTER_URI=http://localhost:11450
+# roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world &
+# # roslaunch turtlebot_teleop keyboard_teleop.launch
+# sleep 12s
+# export ROS_MASTER_URI=http://localhost:11351
+# export GAZEBO_MASTER_URI=http://localhost:11451
+# roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world &
+# sleep12
+# export ROS_MASTER_URI=http://localhost:11352
+# export GAZEBO_MASTER_URI=http://localhost:11452
+# roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+  else
+  if [ $1 -gt 0 ]
+    then
+      for i in $(seq 1 1 $(($1-1)))
+      do
+        export  ROS_MASTER_URI=http://localhost:$(($ros_localhost+$i-1))
+        export  GAZEBO_MASTER_URI=http://localhost:$(($gazebo_localhost+$i-1))
+        roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world &
+        sleep 20s
+      done
+      export ROS_MASTER_URI=http://localhost:$(($ros_localhost+$1-1))
+      export GAZEBO_MASTER_URI=http://localhost:$(($gazebo_localhost+$1-1))
+      roslaunch launch/my_turtlebot_simulator.launch robot:=_extended gui:=false head_controller:=true pause:=false world_file:=/local_scratch/aauterna/git/src/turtlebot_src/turtlebot_simulator/turtlebot_gazebo/worlds/my_turtlebot_world.world
+  fi
+fi
