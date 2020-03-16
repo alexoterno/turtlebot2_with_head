9151e692a4ae8a9f7ada722226cf41954e44d170
Alex <alex.auternaud@inria.fr>
2020-03-16 16:20:18 +0100

    vs update

--- src/turtlebot_src/visual_servoing/src/non_holo_visual_servoing_node.cpp
+++ src/turtlebot_src/visual_servoing/src/non_holo_visual_servoing_node.cpp
@@ -107,7 +107,8 @@ void NonHoloVisualServoing::init_vs(){
     robot_velocities = Eigen::MatrixXd::Zero(4,1);
     camera_velocities = Eigen::MatrixXd::Zero(4,1);
     lambda = Eigen::MatrixXd::Identity(4,4);
-    // lambda(1,1) = 0.9;
+    lambda(0,0) = 0.5;
+    lambda(1,1) = 0.5;
   }
   else if(vs_joints == "pan_tilt"){
     J_Robot_invert = Eigen::MatrixXd::Zero(5, 7); // moore pseudo invert
@@ -225,6 +226,9 @@ void NonHoloVisualServoing::poseCallback(const geometry_msgs::PoseStampedConstPt
     std::cout << "pan_ : " << pan_ << std::endl;
     std::cout << "tilt_ : " << tilt_ << std::endl;
     std::cout << "yaw_ : " << yaw_ << std::endl;
+    std::cout << "yaw_  (deg): " << yaw_*180/3.14159 << std::endl;
+    std::cout << "cos(yaw_) : " << cos(yaw_) << std::endl;
+    std::cout << "sin(yaw_) : " << sin(yaw_) << std::endl;
     // std::cout << "s_x : " << s_x.get_s() << std::endl;
     // std::cout << "s_xd : " << s_xd.get_s()  << std::endl;
     // std::cout << "s_Z x: " << s_Z.get_x()  << std::endl;
@@ -253,180 +257,180 @@ void NonHoloVisualServoing::poseCallback(const geometry_msgs::PoseStampedConstPt
       out_cmd_vel.angular.x = 0;
       out_cmd_vel.angular.y = 0;
       out_cmd_vel.angular.z = 0.1; // turn to find the marker 0.1
-      out_pan_vel.data = pan_;
-      out_tilt_vel.data = tilt_;
+      out_pan_vel.data = 0;
+      out_tilt_vel.data = 0;
       mobile_base_pub_.publish(out_cmd_vel);
-      // head_pan_pub_.publish(out_pan_vel);
-      // head_tilt_pub_.publish(out_tilt_vel);
+      head_pan_pub_.publish(out_pan_vel);
+      head_tilt_pub_.publish(out_tilt_vel);
       valid_pose = false;
       valid_pose_prev = valid_pose;
       return;
     }
-  // Update the current x feature
-  s_x.set_xyZ(origin.p[0], origin.p[1], Z); //Feature coordinates expressed in the image plane p. They correspond to 2D normalized coordinates expressed in meters
-  // $ x $ and $ y $ represent the coordinates of the point in the image plan and are the parameters of the visual feature $ s $. $ Z $ is the 3D coordinate in the camera frame representing the depth
-  // Update log(Z/Z*) feature. Since the depth Z change, we need to update the intection matrix
-  s_Z.buildFrom(s_x.get_x(), s_x.get_y(), Z, log(Z/Zd));
-  // cVe = robot.get_cVe();
-  // task.set_cVe(cVe);
-  // Get the robot jacobian
-  // eJe = robot.get_eJe();
-  // Update the jacobian that will be used to compute the control law
-  // task.set_eJe(eJe);
-  // Compute the control law. Velocities are computed in the mobile robot reference frame
-  v = task.computeControlLaw();
-  // if (0) { //valid_pose_prev == false) {
-  //     // Start a new visual servo
-  //     ROS_INFO("Reinit visual servo");
-  //
-  //     tinit = t_start_loop;
-  //     vi = v;
-  //   }
-  // v = v - vi*exp(-mu*(t_start_loop - tinit)/1000.);
-  if (
-    (std::abs(v[0]) <= min_vel_linear * high_ratio) &&
-    (std::abs(v[2]) <= min_vel_angular * high_ratio) &&
-    (std::abs(v[3]) <= min_vel_angular * high_ratio) &&
-    (std::abs(v[4]) <= min_vel_angular * high_ratio)
-    // (std::abs(v[4]) <= min_vel_angular) && (std::abs(v[5]) <= min_vel_angular)
-  ){
-    std::cout << "velocity" << std::endl;
-    std::cout << v << std::endl;
-    //Send message
-    ROS_INFO("I\'m Done"); //Just to check
-    //Publishing to fit to the multi master process and send signal to next task
-    // pubTalker_.publish(msgDone);
-    exit(1);
-  }
-  if(vs_joints == "pan"){
-    // std::cout << "pan only" << std::endl;
-    J_Robot (0, 0) = -1;
-    J_Robot (0, 1) = -1;
-    J_Robot (1, 0) = -L22-L13;
-    J_Robot (1, 1) = -cos(pan_)*L31+sin(pan_)*L32-L22-L13;
-    J_Robot (1, 2) = -sin(pan_+yaw_);
-    J_Robot (1, 3) = -cos(pan_+yaw_);
-    J_Robot (2, 0) = -L23+L11;
-    J_Robot (2, 1) = -sin(pan_)*L31-sin(pan_)*L32-L22-L13;
-    J_Robot (2, 2) = cos(pan_+yaw_);
-    J_Robot (2, 3) = -sin(pan_+yaw_);
-    J_Robot (3, 2) = sin(yaw_);
-    J_Robot (3, 3) = -cos(yaw_);
-    J_Robot_invert = J_Robot.inverse();
+    // Update the current x feature
+    s_x.set_xyZ(origin.p[0], origin.p[1], Z); //Feature coordinates expressed in the image plane p. They correspond to 2D normalized coordinates expressed in meters
+    // $ x $ and $ y $ represent the coordinates of the point in the image plan and are the parameters of the visual feature $ s $. $ Z $ is the 3D coordinate in the camera frame representing the depth
+    // Update log(Z/Z*) feature. Since the depth Z change, we need to update the intection matrix
+    s_Z.buildFrom(s_x.get_x(), s_x.get_y(), Z, log(Z/Zd));
+    // cVe = robot.get_cVe();
+    // task.set_cVe(cVe);
+    // Get the robot jacobian
+    // eJe = robot.get_eJe();
+    // Update the jacobian that will be used to compute the control law
+    // task.set_eJe(eJe);
+    // Compute the control law. Velocities are computed in the mobile robot reference frame
+    v = task.computeControlLaw();
+    // if (0) { //valid_pose_prev == false) {
+    //     // Start a new visual servo
+    //     ROS_INFO("Reinit visual servo");
+    //
+    //     tinit = t_start_loop;
+    //     vi = v;
+    //   }
+    // v = v - vi*exp(-mu*(t_start_loop - tinit)/1000.);
+    if (
+      (std::abs(v[0]) <= min_vel_linear * high_ratio) &&
+      (std::abs(v[2]) <= min_vel_angular * high_ratio) &&
+      (std::abs(v[3]) <= min_vel_angular * high_ratio) &&
+      (std::abs(v[4]) <= min_vel_angular * high_ratio)
+      // (std::abs(v[4]) <= min_vel_angular) && (std::abs(v[5]) <= min_vel_angular)
+    ){
+      std::cout << "velocity" << std::endl;
+      std::cout << v << std::endl;
+      //Send message
+      ROS_INFO("I\'m Done"); //Just to check
+      //Publishing to fit to the multi master process and send signal to next task
+      // pubTalker_.publish(msgDone);
+      exit(1);
+    }
+    if(vs_joints == "pan"){
+      // std::cout << "pan only" << std::endl;
+      J_Robot (0, 0) = -1;
+      J_Robot (0, 1) = -1;
+      J_Robot (1, 0) = -L22-L13;
+      J_Robot (1, 1) = -cos(pan_)*L31+sin(pan_)*L32-L22-L13;
+      J_Robot (1, 2) = -sin(pan_+yaw_);
+      J_Robot (1, 3) = -cos(pan_+yaw_);
+      J_Robot (2, 0) = -L23+L11;
+      J_Robot (2, 1) = -sin(pan_)*L31-cos(pan_)*L32-L23+L11;
+      J_Robot (2, 2) = cos(pan_+yaw_);
+      J_Robot (2, 3) = -sin(pan_+yaw_);
+      J_Robot (3, 2) = sin(yaw_);
+      J_Robot (3, 3) = -cos(yaw_);
+      J_Robot_invert = J_Robot.inverse();
 
-    camera_velocities (0) = v[4];
-    camera_velocities (1) = v[0];
-    camera_velocities (2) = v[2];
+      camera_velocities (0) = -v[4];
+      camera_velocities (1) = -v[0];
+      camera_velocities (2) = v[2];
 
-    robot_velocities = lambda*J_Robot_invert * camera_velocities;
+      robot_velocities = lambda*J_Robot_invert * camera_velocities;
 
-    out_command(1) = delta_t*robot_velocities(0);
-    out_command(2) = robot_velocities(1);
-    out_command(3) = pow((pow(robot_velocities(2), 2)+pow(robot_velocities(3), 2)),0.5); //robot_velocities(2)*cos(yaw_)+robot_velocities(3)*sin(yaw_);
-  }
-  else if(vs_joints == "pan_tilt"){
-    // std::cout << "pan_tilt" << std::endl;
-    J_Robot (0, 0) = -1;
+      out_command(1) = robot_velocities(0); //delta_t
+      out_command(2) = robot_velocities(1);
+      out_command(3) = pow((pow(robot_velocities(2), 2)+pow(robot_velocities(3), 2)),0.5); // robot_velocities(2)*cos(yaw_)+robot_velocities(3)*sin(yaw_); //
+      std::cout << "out_command" << std::endl;
+      std::cout << out_command << std::endl;
+    }
+    else if(vs_joints == "pan_tilt"){
+      // std::cout << "pan_tilt" << std::endl;
+      J_Robot (0, 0) = -1;
 
-    J_Robot (1, 1) = -cos(tilt_);
-    J_Robot (1, 2) = -cos(tilt_);
+      J_Robot (1, 1) = -cos(tilt_);
+      J_Robot (1, 2) = -cos(tilt_);
 
-    J_Robot (2, 1) = sin(tilt_);
-    J_Robot (2, 2) = sin(tilt_);
+      J_Robot (2, 1) = sin(tilt_);
+      J_Robot (2, 2) = sin(tilt_);
 
-    J_Robot (3, 1) = -cos(tilt_)*L13-sin(tilt_)*L12-L22;
-    J_Robot (3, 2) = -cos(tilt_)*L13-sin(tilt_)*L12-L22-cos(pan_)*L31+sin(tilt_)*L32;
-    J_Robot (3, 3) = -sin(pan_+yaw_);
-    J_Robot (3, 4) = -cos(pan_+yaw_);
+      J_Robot (3, 1) = -cos(tilt_)*L13-sin(tilt_)*L12-L22;
+      J_Robot (3, 2) = -cos(tilt_)*L13-sin(tilt_)*L12-L22-cos(pan_)*L31+sin(tilt_)*L32;
+      J_Robot (3, 3) = -sin(pan_+yaw_);
+      J_Robot (3, 4) = -cos(pan_+yaw_);
 
-    J_Robot (4, 0) = L13;
-    J_Robot (4, 1) = sin(tilt_)*(L11-L23);
-    J_Robot (4, 2) = sin(tilt_)*(-sin(pan_)*L31-cos(tilt_)*L32+L11-L23);
-    J_Robot (4, 3) = sin(tilt_)*cos(pan_+yaw_);
-    J_Robot (4, 4) = -sin(tilt_)*sin(pan_+yaw_);
+      J_Robot (4, 0) = L13;
+      J_Robot (4, 1) = sin(tilt_)*(L11-L23);
+      J_Robot (4, 2) = sin(tilt_)*(-sin(pan_)*L31-cos(tilt_)*L32+L11-L23);
+      J_Robot (4, 3) = sin(tilt_)*cos(pan_+yaw_);
+      J_Robot (4, 4) = -sin(tilt_)*sin(pan_+yaw_);
 
-    J_Robot (5, 0) = -L12;
-    J_Robot (5, 1) = cos(tilt_)*(L11-L23);
-    J_Robot (5, 2) = cos(tilt_)*(-sin(pan_)*L31-cos(tilt_)*L32+L11-L23);
-    J_Robot (5, 3) = cos(tilt_)*cos(pan_+yaw_);
-    J_Robot (5, 4) = -cos(tilt_)*sin(pan_+yaw_);
+      J_Robot (5, 0) = -L12;
+      J_Robot (5, 1) = cos(tilt_)*(L11-L23);
+      J_Robot (5, 2) = cos(tilt_)*(-sin(pan_)*L31-cos(tilt_)*L32+L11-L23);
+      J_Robot (5, 3) = cos(tilt_)*cos(pan_+yaw_);
+      J_Robot (5, 4) = -cos(tilt_)*sin(pan_+yaw_);
 
-    J_Robot (6, 3) = sin(yaw_);
-    J_Robot (6, 4) = -cos(yaw_);
+      J_Robot (6, 3) = sin(yaw_);
+      J_Robot (6, 4) = -cos(yaw_);
 
-    J_Robot_invert = (J_Robot.transpose()*J_Robot).inverse()*J_Robot.transpose(); // Moore pseudo inverse
+      J_Robot_invert = (J_Robot.transpose()*J_Robot).inverse()*J_Robot.transpose(); // Moore pseudo inverse
 
-    camera_velocities (0) = v[3];
-    camera_velocities (1) = v[4];
-    camera_velocities (2) = v[5];
-    camera_velocities (3) = v[0];
-    camera_velocities (4) = v[1];
-    camera_velocities (5) = v[2];
+      camera_velocities (0) = -v[3];
+      camera_velocities (1) = -v[4];
+      camera_velocities (2) = v[5];
+      camera_velocities (3) = -v[0];
+      camera_velocities (4) = -v[1];
+      camera_velocities (5) = v[2];
 
-    robot_velocities = lambda*J_Robot_invert * camera_velocities;
+      robot_velocities = lambda*J_Robot_invert * camera_velocities;
 
-    out_command(0) = delta_t*robot_velocities(0);
-    out_command(1) = delta_t*robot_velocities(1);
-    out_command(2) = robot_velocities(2);
-    out_command(3) = pow((pow(robot_velocities(3), 2)+pow(robot_velocities(4), 2)),0.5);
-  }
-  std::cout << "J_Robot" << std::endl;
-  std::cout << J_Robot << std::endl;
-  std::cout << "lambda" << std::endl;
-  std::cout << lambda << std::endl;
-  std::cout << "J_Robot_invert" << std::endl;
-  std::cout << J_Robot_invert << std::endl;
-  std::cout << "robot_velocities" << std::endl;
-  std::cout << robot_velocities << std::endl;
-  std::cout << "camera_velocities" << std::endl;
-  std::cout << camera_velocities << std::endl;
-  std::cout << "out_command" << std::endl;
-  std::cout << out_command << std::endl;
-  //////////////////////
-  if (std::abs(out_command(3)) > max_linear_vel || std::abs(out_command(0)) > max_angular_vel || std::abs(out_command(1)) > max_angular_vel || std::abs(delta_t*out_command(2)) > max_angular_vel) {
+      out_command(0) = robot_velocities(0); // delta_t
+      out_command(1) = robot_velocities(1); // delta_t
+      out_command(2) = robot_velocities(2);
+      out_command(3) = pow((pow(robot_velocities(3), 2)+pow(robot_velocities(4), 2)),0.5);
+    }
+
     if(std::abs(out_command(3)) > max_linear_vel){
       ROS_INFO("linear speed exceed max allowed");
       out_command(3) = max_linear_vel;
     }
-    else if(std::abs(out_command(0)) > max_angular_vel){
+    if(std::abs(out_command(0)) > max_angular_vel){
       ROS_INFO("Tilt angular spee exceed max allowed");
       out_command(0) = max_angular_vel;
     }
-    else if(std::abs(out_command(1)) > max_angular_vel){
+    if(std::abs(out_command(1)) > max_angular_vel){
       ROS_INFO("Pan angular speed exceed max allowed");
       out_command(1) = max_angular_vel;
     }
-    else if(std::abs(delta_t*out_command(1)) > max_angular_vel){
+    if(std::abs(out_command(2)) > max_angular_vel){
       ROS_INFO("Robot angular speed exceed max allowed");
-      out_command(1) = max_angular_vel;
+      out_command(2) = max_angular_vel;
     }
-  }
-  std::cout << "velocity" << std::endl;
-  std::cout << v << std::endl;
-  out_cmd_vel.linear.x = out_command(3);
-  out_cmd_vel.linear.y = 0;
-  out_cmd_vel.linear.z = 0;
-  out_cmd_vel.angular.x = 0;
-  out_cmd_vel.angular.y = 0;
-  out_cmd_vel.angular.z = out_command(2);
-  pan_ += out_command(1);
-  out_pan_vel.data = pan_;
-  tilt_ += out_command(0);
-  out_tilt_vel.data = tilt_;
 
-  mobile_base_pub_.publish(out_cmd_vel);
-  head_pan_pub_.publish(out_pan_vel);
-  if(vs_joints == "pan_tilt"){
-    head_tilt_pub_.publish(out_tilt_vel);
-  }
-  valid_pose_prev = valid_pose;
-  valid_pose = false;
-  std::cout << "out_cmd_vel : " << out_cmd_vel << std::endl;
-  std::cout << "out_pan_vel : " << out_pan_vel << std::endl;
-  std::cout << "out_tilt_vel : " << out_tilt_vel << std::endl;
-  }
+    std::cout << "J_Robot" << std::endl;
+    std::cout << J_Robot << std::endl;
+    std::cout << "lambda" << std::endl;
+    std::cout << lambda << std::endl;
+    std::cout << "J_Robot_invert" << std::endl;
+    std::cout << J_Robot_invert << std::endl;
+    std::cout << "robot_velocities" << std::endl;
+    std::cout << robot_velocities << std::endl;
+    std::cout << "camera_velocities" << std::endl;
+    std::cout << camera_velocities << std::endl;
+    std::cout << "out_command" << std::endl;
+    std::cout << out_command << std::endl;
+    std::cout << "velocity" << std::endl;
+    std::cout << v << std::endl;
+    out_cmd_vel.linear.x = out_command(3);
+    out_cmd_vel.linear.y = 0;
+    out_cmd_vel.linear.z = 0;
+    out_cmd_vel.angular.x = 0;
+    out_cmd_vel.angular.y = 0;
+    out_cmd_vel.angular.z = out_command(2);
+    // pan_ += out_command(1);
+    out_pan_vel.data = out_command(1);//pan_;
+    // tilt_ += out_command(0);
+    out_tilt_vel.data = out_command(0);//tilt_;
 
-  catch(...) {
+    mobile_base_pub_.publish(out_cmd_vel);
+    head_pan_pub_.publish(out_pan_vel);
+    if(vs_joints == "pan_tilt"){
+      head_tilt_pub_.publish(out_tilt_vel);
+    }
+    valid_pose_prev = valid_pose;
+    valid_pose = false;
+    std::cout << "out_cmd_vel : " << out_cmd_vel << std::endl;
+    std::cout << "out_pan_vel : " << out_pan_vel << std::endl;
+    std::cout << "out_tilt_vel : " << out_tilt_vel << std::endl;
+    }
+  catch(...){
     ROS_INFO("Catch an exception: set vel to 0");
     out_cmd_vel.linear.x = 0;
     out_cmd_vel.linear.y = 0;
@@ -434,11 +438,11 @@ void NonHoloVisualServoing::poseCallback(const geometry_msgs::PoseStampedConstPt
     out_cmd_vel.angular.x = 0;
     out_cmd_vel.angular.y = 0;
     out_cmd_vel.angular.z = 0;
-    out_pan_vel.data = pan_;
-    out_tilt_vel.data = tilt_;
+    out_pan_vel.data = 0;
+    out_tilt_vel.data = 0;
     mobile_base_pub_.publish(out_cmd_vel);
-    // head_pan_pub_.publish(out_pan_vel);
-    // head_tilt_pub_.publish(out_tilt_vel);
+    head_pan_pub_.publish(out_pan_vel);
+    head_tilt_pub_.publish(out_tilt_vel);
   }
 }
 
